354cbff52350355b16033ccdaadbba2010c3051d
fehoon.Hyeon <clearch21@gmail.com>
2025-02-15 11:12:13 +0900

    Initial commit

---
+++ src/main/java/com/bc/msa/product/HomeController.java
@@ -0,0 +1,13 @@
+package com.bc.msa.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public RedirectView redirectToSwagger() {
+        return new RedirectView("/swagger-ui.html");
+    }
+}
